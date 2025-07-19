c686975c00a0a8f230b5beeaf8c712143e75cf41
Gourish naik <gourish.naik@tiniva.com>
2025-07-19 20:13:06 +0530

    "changes"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stractural-direvatives';
+  show:boolean=false;
+  Fruits = ['Banana','Apple','Pineapple'];
+  status = 'error'
+  
 }
